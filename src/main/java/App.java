027e56f1b2c766d47eea64225ce25d3f71f78572
Valery <valeryroj@yandex.ru>
2024-03-22 22:42:15 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(secondBean.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat SecondCatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(SecondCatBean.getName());
+
+        System.out.println(bean == secondBean);
+        System.out.println(catBean == SecondCatBean);
     }
 }
